5645335da148319e60a6ccbcc94b186755d0a5f2
agrimas19 <58232240+agrimas19@users.noreply.github.com>
2019-11-26 14:28:49 -0500

    Add files via upload

---
+++ SpringREST.java
@@ -0,0 +1,13 @@
+package com.springREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringREST {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringREST.class,args);
+	}
+}
